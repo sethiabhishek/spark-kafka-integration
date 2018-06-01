a15c8725ad5147d903594daedf922193812d27af
Abhishek Sethi <abhishek.sethi@nagarro.com>
2018-06-01 15:08:52 +0530

    updated code

--- src/main/scala/kafka/UserPaymentsJob.scala
+++ src/main/scala/kafka/UserPaymentsJob.scala
@@ -12,7 +12,6 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.json4s.DefaultFormats
 import org.json4s.jackson.Serialization.read
 
-
 object UserPaymentsJob extends App {
 
   case class RecordBean(ts: String, userid: String, eventid: String)
@@ -21,7 +20,7 @@ object UserPaymentsJob extends App {
 
   val sparkConf = new SparkConf().setAppName("User-Payment-Integration").setMaster("local[*]")
   val spark = SparkSession.builder.config(sparkConf).getOrCreate()
-  val ssc = new StreamingContext(spark.sparkContext, Seconds(30))
+  val ssc = new StreamingContext(spark.sparkContext, Seconds(60))
 
   // Retrieving the details of the broker and the topic to connect
   val config = ConfigFactory.load()
@@ -31,20 +30,14 @@ object UserPaymentsJob extends App {
   // alias for simplicity
   type Record = ConsumerRecord[String, String]
 
-  // publishing the schema type for further analysis
-  val userSchema = StructType(Array(StructField("ts", StringType), StructField("userid", StringType), StructField("eventid", StringType)))
-
   val kafkaParams = Map[String, Object]("bootstrap.servers" -> broker, "key.deserializer" -> classOf[StringDeserializer], "value.deserializer" -> classOf[StringDeserializer], "group.id" -> "bookinggroup", "auto.offset.reset" -> "latest", "enable.auto.commit" -> (false: java.lang.Boolean))
 
   val topics = Array("bookings")
 
   val stream = KafkaUtils.createDirectStream(ssc, LocationStrategies.PreferConsistent, ConsumerStrategies.Subscribe[String, String](topics, kafkaParams))
 
-  val userEventsRDD: Unit = stream.foreachRDD((rdd: RDD[ConsumerRecord[String, String]]) => {
-    // convert string to PoJo and generate rows as tuple group
-    val pairs = rdd.map(row => (row.timestamp(), read[RecordBean](row.value()))).map(row => (row._2.userid, row._2.eventid))
-    pairs.foreachPartition(itr => print(itr.next()))
-  })
+  val modified = stream.map(cons => read[RecordBean](cons.value())).map(record => (record.userid, record.eventid))
+  modified.reduceByKey((r1, r2) => r1 + "," + r2).filter(!_._2.contains("SubmitPayment")).map(_._1).print()
 
   ssc.start()
   ssc.awaitTermination()
